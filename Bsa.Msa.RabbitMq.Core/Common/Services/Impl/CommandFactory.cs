3b86842561b36200c15c1bb41c972fa1fb7d6ba0
Stanislav Batyrgareev <62818625+S-DyX@users.noreply.github.com>
2026-04-20 14:58:09 +0300

    Добавил логирование

--- Bsa.Msa.RabbitMq.Core/Common/Services/Impl/CommandFactory.cs
+++ Bsa.Msa.RabbitMq.Core/Common/Services/Impl/CommandFactory.cs
@@ -3,7 +3,6 @@ using Bsa.Msa.Common.Services.Interfaces;
 using Bsa.Msa.Common.Settings;
 using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Threading;
 
 namespace Bsa.Msa.Common.Services.Impl
@@ -12,57 +11,71 @@ namespace Bsa.Msa.Common.Services.Impl
 	public sealed class CommandFactory : ICommandFactory
 	{
 		private readonly ICommandRegistry commandRegistry;
+		private readonly ILocalLogger _localLogger;
 		private readonly ILocalContainer localContainer;
 
 		/// <summary>
 		/// Ctor
 		/// </summary>
 		/// <param name="commandRegistry"></param>
+		/// <param name="localLogger"></param>
 		/// <param name="localContainer"></param>
-		public CommandFactory(ICommandRegistry commandRegistry, ILocalContainer localContainer = null)
+		public CommandFactory(ICommandRegistry commandRegistry, ILocalLogger localLogger = null, ILocalContainer localContainer = null)
 		{
 			this.commandRegistry = commandRegistry;
+			_localLogger = localLogger;
 			this.localContainer = localContainer;
 		}
 
 		/// <inheritdoc />
 		public ICommand Create(string commandType, ISettings settings, CancellationToken cancellationToken)
 		{
-			var handlerType = commandRegistry.Resolve(commandType);
-			if (handlerType == null)
-				throw new InvalidOperationException($"Type not found {commandType}");
-			var constructor = handlerType.GetConstructor(Type.EmptyTypes);
-			if (constructor != null)
-				return Activator.CreateInstance(handlerType) as ICommand;
-
-			var constructors = handlerType.GetConstructors();
-			var result = new List<object>();
-			foreach (var c in constructors)
+			Type handlerType = null;
+			try
 			{
-				var parameters = c.GetParameters();
-				foreach (var p in parameters)
+				handlerType = commandRegistry.Resolve(commandType);
+				if (handlerType == null)
+					throw new InvalidOperationException($"Type not found {commandType}");
+				var constructor = handlerType.GetConstructor(Type.EmptyTypes);
+				if (constructor != null)
+					return Activator.CreateInstance(handlerType) as ICommand;
+
+				var constructors = handlerType.GetConstructors();
+				var result = new List<object>();
+				foreach (var c in constructors)
 				{
-					if (p.ParameterType == typeof(ISettings))
+					var parameters = c.GetParameters();
+					foreach (var p in parameters)
 					{
-						result.Add(settings);
-					}
-					else
-					{
-						var inst = localContainer.Resolve(p.ParameterType);
-						if (inst != null)
+						if (p.ParameterType == typeof(ISettings))
 						{
-							result.Add(inst);
+							result.Add(settings);
 						}
 						else
 						{
-							throw new InvalidOperationException($"Parameter not found {p.ParameterType.Name} {p.Name}");
+							var inst = localContainer.Resolve(p.ParameterType);
+							if (inst != null)
+							{
+								result.Add(inst);
+							}
+							else
+							{
+								throw new InvalidOperationException($"Parameter not found {p.ParameterType.Name} {p.Name}");
+							}
 						}
 					}
-				}
 
+				}
+				object[] args = result.ToArray();
+				var instance = Activator.CreateInstance(handlerType, args) as ICommand;
+				return instance;
 			}
-			object[] args = result.ToArray();
-			return Activator.CreateInstance(handlerType, args) as ICommand;
+			catch (Exception e)
+			{
+				_localLogger.Error($"Can not create instance of command type:{commandType}; TypeOf:{handlerType}; Message:{e.Message}", e);
+				throw;
+			}
+
 
 
 		}
